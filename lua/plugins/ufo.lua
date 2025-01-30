b9177a22d96809840877ace3b70e299d4ae00108
AT <avi@Tumus-MacBook-Air.local>
2025-01-30 13:25:40 +0530

    updated ufo and deleted Filepath.lua

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,25 +1,56 @@
-return{
+return {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
-    config = function ()
-        vim.o.foldcolumn = '1' -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    config = function()
+        vim.o.foldcolumn = '1'
+        vim.o.foldlevel = 99
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-        require('ufo').setup({
-            vim.keymap.set('n', 'zR', require('ufo').openAllFolds);
-            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds);
-            vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds);
-            vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith); -- closeAllFolds == closeFoldsWith(0)
-            vim.keymap.set('n', 'K', function()
-                local winid = require('ufo').peekFoldedLinesUnderCursor()
-                if not winid then
-                    vim.lsp.buf.hover()
+
+        local handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    local hlGroup = chunk[2]
+                    table.insert(newVirtText, {chunkText, hlGroup})
+                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    if curWidth + chunkWidth < targetWidth then
+                        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                    end
+                    break
                 end
-            end);
+                curWidth = curWidth + chunkWidth
+            end
+            table.insert(newVirtText, {suffix, 'MoreMsg'})
+            return newVirtText
+        end
+
+        require('ufo').setup({
+            fold_virt_text_handler = handler,
             provider_selector = function(bufnr, filetype, buftype)
                 return {'treesitter', 'indent'}
             end
         })
+
+        -- Keymaps should be outside the setup table
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+        vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith)
+        vim.keymap.set('n', 'K', function()
+            local winid = require('ufo').peekFoldedLinesUnderCursor()
+            if not winid then
+                vim.lsp.buf.hover()
+            end
+        end)
     end
 }
