191c34864085e9471be24e11ae227b10182ea3e6
AT <avi@Tumus-MacBook-Air.local>
2025-01-30 12:49:01 +0530

    updated lua/plugins/statusline.lua deleted lua/sttsline_component/Filepath.lua

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,41 +1,40 @@
-return
-    {
-        "sontungexpt/sttusline",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        event = { "BufEnter" },
-        config = function(_)
-            require("sttusline").setup {
-                -- statusline_color = "#000000",
-                statusline_color = "StatusLine",
+return {
+  {
+    "sontungexpt/sttusline",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    event = { "BufEnter" },
 
-                -- | 1 | 2 | 3
-                -- recommended: 3
-                laststatus = 3,
-                disabled = {
-                    filetypes = {
-                        -- "NvimTree",
-                        -- "lazy",
-                    },
-                    buftypes = {
-                        -- "terminal",
-                    },
-                },
-                components = {
-                    "mode",
-                    "filename",
-                    "git-branch",
-                    "git-diff",
-                    "%=",
-                    "diagnostics",
-                    "lsps-formatters",
-                    "copilot",
-                    "indent",
-                    "encoding",
-                    "pos-cursor",
-                    "pos-cursor-progress",
-                },
-            }
-        end,
-    }
+      -- Then setup the statusline
+      require("sttusline").setup {
+        laststatus = 2,
+        statusline_color = "StatusLine",
+        disabled = {
+          filetypes = {
+            -- "NvimTree",
+            -- "lazy",
+          },
+          buftypes = {
+            -- "terminal",
+          },
+        },
+        components = {
+          "mode",
+          "filename",
+          "git-branch",
+          "git-diff",
+          "datetime",
+          "%=",
+          "diagnostics",
+          "lsps-formatters",
+          "copilot",
+          "indent",
+          "encoding",
+          "pos-cursor",
+          "pos-cursor-progress",
+        },
+      }
+    end,
+  }
+}
