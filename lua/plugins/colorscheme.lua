6770ad5b6acc04113fe619988fe18f0d25ce6c57
avi tumu <avi@avis-MacBook-Air.local>
2025-04-08 08:53:50 +0530

    added transparent colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,8 @@ return {
     priority = 1000, -- Higher priority, loads earlier
     config = function()
         vim.cmd "colorscheme kanagawa-dragon"
+        require('kanagawa').setup({
+            transparent=true
+        })
     end,
 }
